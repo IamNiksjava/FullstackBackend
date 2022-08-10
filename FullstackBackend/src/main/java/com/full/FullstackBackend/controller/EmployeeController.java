ec8b13dff74b83ee59a6981db28324b8881f7070
IamNiksjava <nchaudhari677@gmail.com>
2022-08-10 21:34:47 +0530

    @autowired added

--- FullstackBackend/src/main/java/com/full/FullstackBackend/controller/EmployeeController.java
+++ FullstackBackend/src/main/java/com/full/FullstackBackend/controller/EmployeeController.java
@@ -3,17 +3,22 @@ package com.full.FullstackBackend.controller;
 
 import com.full.FullstackBackend.model.Employee;
 import com.full.FullstackBackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
+    @Autowired
     private EmployeeRepository repository;
 
+
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return repository.findAll();
 
